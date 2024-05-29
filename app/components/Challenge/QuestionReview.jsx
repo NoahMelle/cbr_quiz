2b26a78809f280745fc95148263fb81919ec903d
NoahMelle <noahmkamphuisen@gmail.com>
2024-05-29 16:21:33 +0200

    verder button werkt weer

--- app/components/Challenge/QuestionReview.jsx
+++ app/components/Challenge/QuestionReview.jsx
@@ -1,18 +1,24 @@
 import React from "react";
-export default function QuestionReview({ isCorrect  ,  setCurrentQuestion  ,  currentQuestion  ,  setQuestionsAnswered  ,  questionsAnswered  , setScore, score  }){
-    function nextQuestion(){
+export default function QuestionReview({
+    isCorrect,
+    setCurrentQuestion,
+    currentQuestion,
+    setQuestionsAnswered,
+    questionsAnswered,
+    setScore,
+    score,
+}) {
+    function nextQuestion(e) {
         setCurrentQuestion(currentQuestion + 1);
         setQuestionsAnswered(questionsAnswered + 1);
         setScore(score + 100);
     }
 
     return (
-        <form action="#" onSubmit={nextQuestion}>
         <div>
             <p>{isCorrect ? "correct" : "incorrect"}</p>
             <p>de feedback </p>
-            <button>Verder</button>
+            <button type="button" onClick={nextQuestion}>Verder</button>
         </div>
-        </form>
-    )
+    );
 }
